4d616a1f3472aa43f5ce6914f6d04a5a6054066e
Olivia Cubela <oliviacubela@gmail.com>
2020-06-03 17:53:02 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-array = ["red", "yellow", "arrays!"]
-array.unshift("green")
+array = ["red", "yellow", "blue"]
+array << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
